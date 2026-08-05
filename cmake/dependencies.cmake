df22361983379c90509e23016f5b01ab9d029ba8
Ren Davis <rcdavisgd@gmail.com>
2026-08-05 03:34:25 -0400

    Removed unnecessary defines for VMA

--- cmake/dependencies.cmake
+++ cmake/dependencies.cmake
@@ -27,7 +27,7 @@ set(SDL_TEST_LIBRARY OFF CACHE BOOL "" FORCE)
 FetchContent_Declare(
     SDL3
     GIT_REPOSITORY https://github.com/libsdl-org/SDL.git
-    GIT_TAG release-3.4.12
+    GIT_TAG release-3.4.14
 )
 FetchContent_MakeAvailable(SDL3)
 
@@ -37,9 +37,6 @@ FetchContent_Declare(
     GIT_TAG 1.4.350
 )
 FetchContent_MakeAvailable(volk)
-
-set(VMA_STATIC_VULKAN_FUNCTIONS OFF CACHE BOOL "" FORCE)
-set(VMA_DYNAMIC_VULKAN_FUNCTIONS ON CACHE BOOL "" FORCE)
 
 FetchContent_Declare(
     vma
